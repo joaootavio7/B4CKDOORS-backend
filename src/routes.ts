e533c66470f335cf606d2a422a210a651496c633
RENAN VINÍCIUS DA SILVA MATOS <renan.matos@portalsesisp.org.br>
2026-03-11 16:44:59 -0300

    chore: configura rotas da API

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,51 @@
-import express from "express";
+import type { Request, Response } from "express";
+import { Router } from "express";
 
-export const router = express.Router();
+import UsuarioController   from "./controller/UsuarioController.js";
+import CategoriaController from "./controller/CategoriaController.js";
+import ProdutoController   from "./controller/ProdutoController.js";
+import PedidoController    from "./controller/PedidoController.js";
 
-router.get("/api", (req, res) => {
-  res.status(200).json({ message: "API Grená funcionando 🚀" });
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ mensagem: "Se você está vendo essa mensagem, seu servidor está funcionando." });
 });
+
+/**
+ * Endpoints para Usuários
+ */
+router.get("/api/usuarios",              UsuarioController.todos);
+router.get("/api/usuarios/:idUsuario",   UsuarioController.usuario);
+router.post("/api/usuarios",             UsuarioController.novo);
+router.put("/api/usuarios/:idUsuario",   UsuarioController.atualizar);
+router.delete("/api/usuarios/:idUsuario", UsuarioController.remover);
+
+/**
+ * Endpoints para Categorias
+ */
+router.get("/api/categorias",                 CategoriaController.todos);
+router.get("/api/categorias/:idCategoria",    CategoriaController.categoria);
+router.post("/api/categorias",                CategoriaController.novo);
+router.put("/api/categorias/:idCategoria",    CategoriaController.atualizar);
+router.delete("/api/categorias/:idCategoria", CategoriaController.remover);
+
+/**
+ * Endpoints para Produtos
+ */
+router.get("/api/produtos",              ProdutoController.todos);
+router.get("/api/produtos/:idProduto",   ProdutoController.produto);
+router.post("/api/produtos",             ProdutoController.novo);
+router.put("/api/produtos/:idProduto",   ProdutoController.atualizar);
+router.delete("/api/produtos/:idProduto", ProdutoController.remover);
+
+/**
+ * Endpoints para Pedidos
+ */
+router.get("/api/pedidos",             PedidoController.todos);
+router.get("/api/pedidos/:idPedido",   PedidoController.pedido);
+router.post("/api/pedidos",            PedidoController.novo);
+router.put("/api/pedidos/:idPedido",   PedidoController.atualizar);
+router.delete("/api/pedidos/:idPedido", PedidoController.remover);
+
+export { router };
